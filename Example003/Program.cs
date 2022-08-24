8e6a5bc256478d46dd095533971035cd2cd1c8d4
Kamoljon <kamol.muminov.98@mail.ru>
2022-08-24 22:01:18 +0300

    gitignore

---
+++ Example003/Program.cs
@@ -0,0 +1,3 @@
+﻿double numberA = 36;
+double numberB = 10;
+Console.WriteLine(numberA/numberB);
---
+++ Example004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ввведите ваше имя: ");
+String username = Console.ReadLine();
+if (username.ToLower() == "masha") {
+    Console.WriteLine(" Hello, masha!");
+}
+else {Console.WriteLine("Hi,");
+Console.Write(username);}
